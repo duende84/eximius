b9072b91a1f685741549fe26f785ae37fc8bff62
duende84 <andreselduende@gmail.com>
2013-04-25 14:13:34 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eximius::Application.config.secret_token = '8b8be9c8ee1b32daf7df33b5e93be49920bfcb57b8f5d22c835fcdd1172e770996f0f2d80df9529962a341158deab44bbffda24b0aa0c507bae08cde09442387'
