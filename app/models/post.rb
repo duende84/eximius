d8597d9ad2ad519d57657ed1130bb7942ddc8a4d
duende84 <andreselduende@gmail.com>
2013-04-29 13:13:37 -0500

    Fix validate length in post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :date, :title, :image, :remote_image_url
 
-  validates :title, presence: true, length: { maximum: 50 }
+  validates :title, presence: true
   validates :content, presence: true
   validates :date, presence: true
 
